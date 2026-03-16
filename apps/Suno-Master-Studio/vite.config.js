9e2657e21cfd200cbf2dc564b4965154ec03c528
saeedalsuri <saeedalsuri@gmail.com>
2026-03-16 11:50:19 +0400

    Remove non-working Storyboard Pro and Suno Master Studio (including large executables)

--- apps/Suno-Master-Studio/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // CRITICAL for Electron: Use relative paths for assets
-})
